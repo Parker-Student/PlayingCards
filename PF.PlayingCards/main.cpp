6e322e51675846da7144cb3eec0bccd4c3161bc0
Foord <700214020@fvtc.edu>
2019-02-04 12:07:54 -0600

    done with part 1

---
+++ PF.PlayingCards/main.cpp
@@ -0,0 +1,59 @@
+//Playing Cards
+// Parker Foord
+
+#include <iostream>
+#include <conio.h>
+#include <string>
+using namespace std;
+
+enum rank {
+
+	two = 2;
+three;
+four;
+five;
+six;
+seven;
+eight;
+nine;
+ten;
+jack;
+queen;
+king;
+ace;
+};
+
+enum suit {
+	club;
+spade;
+heart;
+diamond;
+
+};
+
+struct Card() {
+
+
+}
+
+int main() {
+	Card.card1;
+	card1.suit = heart;
+	card1.rank = two;
+
+
+	cout << "Enter a rank:  ";
+	cin >> i;
+	crad1.rank = (rank)i;
+	cout << card1.rank;
+
+
+	_getch();
+	return 0;
+
+}
+
+
+
+
+
